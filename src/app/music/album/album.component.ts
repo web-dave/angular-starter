3f1b320f747c601b13e0f11ad2776457e70d6fd1
David Muellerchen <dave@webdave.de>
2017-05-29 16:59:22 +0200

    dummy

--- src/app/music/album/album.component.ts
+++ src/app/music/album/album.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.scss']
 })
 export class AlbumComponent implements OnInit {
-
+album;
   constructor() { }
 
   ngOnInit() {
+    this.album={
+    album_type : '',
+    artists : '',
+    available_markets : [],
+    external_urls : {
+      spotify : ''
+    },
+    href :  '',
+    id :  '',
+    images : [],
+    name : '',
+    type : '',
+    uri : '',
+    tracks:{
+        items:[
+            {track_number: 1,
+            name: 'Fooooo',
+            duration_ms : 123456789
+            }
+
+        ]
+    }
+}
   }
 
 }
